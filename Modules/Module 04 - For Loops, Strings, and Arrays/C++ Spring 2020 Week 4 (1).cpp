2f92dbe619f15a03cc25e1ed6b0b1c0aaff7ef41
makavaile <60615251+makavaile@users.noreply.github.com>
2020-02-10 20:38:44 -0600

    Commit

--- Modules/Module 04 - For Loops, Strings, and Arrays/C++ Spring 2020 Week 4 (1).cpp
+++ Modules/Module 04 - For Loops, Strings, and Arrays/C++ Spring 2020 Week 4 (1).cpp
@@ -56,8 +56,102 @@ int main() {
 			case 2: {
 				// don't forget to include statement
 				cout << "All about strings!\n";
+        string playerName = "Mike";
+        cout << "A string is a array of charaters.\n";
+        cout << "The first letter in the players name is \n";
+        cout <<  playerName[0] << ".\n";  // computers start couning at 0
+        cout << "The 2nd letter in the players name is \n" <<  playerName[1] << ".\n";
+        cout << "The 3rd letter in the players name is \n" <<  playerName[2] << ".\n";
+        cout << "The 4th letter in the players name is \n" <<  playerName[3] << ".\n";
+        cout << "The 5th letter in the players name is \n" <<  playerName[4] << ".\n";
+
+        cout << "Using a for loop to add a space between each letter of the player's name.\n";
+        cout << "Also use playerName.size() [" << playerName.size() << "].\n";
+        for(int i = 0; i < playerName.size(); i++) {
+          cout << playerName[i] << " ";
+        }
+
 				break;
-			}	
+			}
+      case 3: {
+        cout << "More strings.\n";
+        string phrase = "Search your feeling";
+
+        cout << phrase << "\n";
+
+        phrase[15] = 'r';
+        cout << phrase << "\n";
+        if(phrase.find("your") == string::npos) {
+          cout << "The phrase 'your' is not in the phrase.\n";
+        } else {
+          // find the spot where its starts
+          int startLocation = phrase.find("your");
+          phrase[startLocation++] = 'm';
+          phrase[startLocation++] = 'y';
+          phrase.erase(startLocation, 2);
+          cout << phrase << "\n";
+        }
+
+        cout << "Erasing the entire phrase...\n";
+        phrase.erase();
+
+        string firstName = "Darth";
+        string lastName = "Vader";
+        phrase = firstName + " " + lastName + "!!!";
+        cout << phrase << "\n";
+
+        break;
+      }
+      case 4: {
+        cout << "Hello Arrays!\n";
+        // new 'list' of strings, size of 5
+        string inventory[5];
+
+        int totalItems = 0;
+        inventory[totalItems++] = "Mustard";
+        inventory[totalItems++] = "EyeDrops";
+        inventory[totalItems++] = "GuitarHero3";
+
+        cout << "Here are your items!\n";
+        for(int i = 0; i < totalItems; i++) {
+          cout << i + 1 << ". " << inventory[i] << "\n";
+        }
+
+        cout << "You stop for lunch, use that mustard.\n";
+        inventory[0] = "Empty Mustard Packet";
+
+        cout << "Here are your items!\n";
+        for(int i = 0; i < totalItems; i++) {
+          cout << i + 1 << ". " << inventory[i] << "\n";
+        }
+
+        cout << "You find another item in the store\n";
+        // add another item to the inventory, and show your inventory
+
+        inventory[totalItems++] = "Cheetos";
+        cout << "You also throw away your empty mustard packet.\n";
+        inventory[0] = "";
+
+        cout << "What do you want to search for in your inventory?\n";
+        string playerInput;
+        cin >> playerInput;
+
+        for(int i = 0; i < totalItems; i++) {
+          if(inventory[i] == playerInput) {
+            cout << "You have " << playerInput << "!\n";
+            cout << "You eat the " << playerInput << " immediately!!\n";
+            inventory[i] = "Empty bag of " + playerInput;
+          }
+          if(inventory[i] != "") {
+            cout <<"- " << inventory[i] << "\n";
+          }
+          else {
+            continue;
+          }
+        }
+
+        break;
+      }
 			default: {
 				cout << "That is not one of the options! Please try again!\n";
 				break;
