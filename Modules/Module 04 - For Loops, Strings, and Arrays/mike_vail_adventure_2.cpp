05b50b4031e3a4c9bbd64b40856ff62011fa28c2
makavaile <60615251+makavaile@users.noreply.github.com>
2020-02-15 16:25:25 -0600

    Commit

--- Modules/Module 04 - For Loops, Strings, and Arrays/mike_vail_adventure_2.cpp
+++ Modules/Module 04 - For Loops, Strings, and Arrays/mike_vail_adventure_2.cpp
@@ -15,3 +15,90 @@
             The player will have to search for that item inside of a for loop.
 */
 #include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main () {
+	srand(time(0));
+
+	cout << "You start your adventure in the forest of Wichita.\n";
+	cout << "You have the following items in your inventory.\n";
+
+// list current items
+	string inv[10];
+	int invItems = 0;
+	inv[invItems++] = "Bow";
+	inv[invItems++] = "Axe";
+	inv[invItems++] = "Sword";
+
+	cout << "\nCurrent Inventory: \n";
+	for(int i = 0; i < invItems; i++) {
+		cout << i + 1 << ". " << inv[i] << endl;
+	}
+
+// start of an encounter
+cout << "As you walk down the path you see a bright light.\n";
+cout << "The light begins to turn a bright red and moves closer.\n";
+cout << "The light shoots fire in your direction. You must protect yourself.\n";
+cout << "What weapon would you like to use?\n";
+
+// list inventory with added item
+	cout << "\nCurrent Inventory: \n";
+	for(int i = 0; i < invItems; i++) {
+		cout << i + 1 << ". " << inv[i] << endl;
+	}
+
+
+
+	for(int i = 0; i < invItems; i++) {
+		string weaponPick;
+		cin >> weaponPick;
+		if(inv[invItems] == weaponPick) {
+			cout << "You take out your " << weaponPick << "!\n";
+			break;
+		}
+		if(inv[invItems] != weaponPick) {
+			cout <<"You don't have that!\n";
+		}
+		else {
+			continue;
+		}
+	}
+
+cout << "You pick up a shield. What would you like to call it?\n";
+
+// adding an item to inventory
+	string input;
+	cin >> input;
+	inv[invItems++] = input;
+
+// list inventory with added item
+	cout << "\nCurrent Inventory: \n";
+	for(int i = 0; i < invItems; i++) {
+		cout << i + 1 << ". " << inv[i] << endl;
+	}
+
+// search for an item
+cout << "What do you want to search for in your inventory?\n";
+string playerInput;
+cin >> playerInput;
+
+	for(int i = 0; i < invItems; i++) {
+		if(inv[i] == playerInput) {
+			cout << "You have " << playerInput << "!\n";
+			cout << "You hand over " << playerInput << " to the troll.\n";
+			inv[invItems] = playerInput;
+			// list inventory with removed item
+				cout << "\nCurrent Inventory: \n";
+				for(int i = 0; i < invItems; i++) {
+					cout << i + 1 << ". " << inv[i] << endl;
+				}
+		}
+
+		else {
+			continue;
+		}
+	}
+}
